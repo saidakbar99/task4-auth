1c0306277c0baab87a1aa53b053bbb0a608f08ae
Saidakbar Zafarov <saidakbar.dev@gmail.com>
2023-12-22 17:06:26 +0500

    Added api services for client folder

---
+++ client/src/services/AuthService.js
@@ -0,0 +1,15 @@
+import $api from "../http";
+
+export default class AuthService {
+    static async login(username, password) {
+        return $api.post('/login', { username, password })
+    }
+
+    static async registration(username, password, email) {
+        return $api.post('/registration', { username, password, email })
+    }
+
+    static async logout() {
+        return $api.post('/logout')
+    }
+}
